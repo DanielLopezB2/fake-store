2cb5b606640043fe50ca63409a99a5ae4fa15af4
Daniel Lopez <dalobe56@gmail.com>
2025-08-11 22:33:19 -0500

    fix: some style details in products details component

--- app/products/components/ProductDetails.vue
+++ app/products/components/ProductDetails.vue
@@ -4,20 +4,30 @@
       {{ product?.title }}
     </h3>
     <div class="my-4 flex justify-center items-center gap-2">
-      <span>Categorias:</span>
+      <span>Categoría:</span>
       <u-badge color="success" :label="product?.category" />
     </div>
-    <div class="px-8 py-6 border border-dashed border-secondary-300 rounded-lg">
+    <div class="px-8 py-6 border border-dashed border-secondary-300 rounded-lg bg-secondary-100/30">
       <nuxt-img :src="product?.image" alt="Product Image" :width="250" />
     </div>
     <div class="flex justify-center mt-8 items-center gap-12 max-w-2xl">
       <div class="flex flex-col">
         <div>
-          <p class="font-semibold text-lg text-primary-300">Description:</p>
+          <p class="font-semibold text-lg text-primary-300">Descripción:</p>
           <p class="text-gray-400">{{ product?.description }}</p>
         </div>
-        <div class="mt-4">
-          <p class="text-primary-400 font-bold text-2xl">${{ product?.price }}</p>
+        <div class="mt-2">
+          <div class="flex items-center gap-2 justify-between">
+            <p class="font-semibold text-lg text-primary-300">Calificación:</p>
+            <p class="text-gray-400 text-sm">{{ product?.rating?.rate }} / 5</p>
+          </div>
+          <u-progress v-model="progressValue" :max="5" color="success" />
+        </div>
+        <div class="mt-8 flex justify-between items-center">
+          <p class="text-primary-500 font-bold text-2xl">${{ product?.price }}</p>
+          <u-button variant="outline" icon="i-lucide-shopping-cart" size="lg">
+            Agregar al carrito
+          </u-button>
         </div>
       </div>
     </div>
@@ -26,5 +36,6 @@
 
 <script setup lang="ts">
   import type { Product } from '../interfaces/product.interface';
-  defineProps<{ product: Product }>();
+  const props = defineProps<{ product: Product }>();
+  const progressValue = ref(props.product?.rating?.rate || 0);
 </script>
